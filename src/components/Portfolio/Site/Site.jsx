11fce52538c7185c3537ee76bfaf4896b8721c13
muhammadsiddiq-pyxel <breakingreport4@gmail.com>
2021-06-07 16:13:49 +0500

    update

--- src/components/Portfolio/Site/Site.jsx
+++ src/components/Portfolio/Site/Site.jsx
@@ -1,8 +1,8 @@
 export const Site = ({img, title, text, githublink,sitelink,classNames})=>{
     return(
-        <div className={`d-flex ${classNames} justify-content-between sites`}>
+        <div className={`d-flex ${classNames} justify-content-between sites my-3`}>
         <div className="my-site">
-            <img src={img} alt="" className="my-site-img" />
+            <img src={img} alt="" className="my-site-img" style={{borderRadius: ".5rem", boxShadow: "3px 4px 10px #333"}} />
         </div>
         <div className="my-site-text d-flex flex-column">
             <h1 className="h3">
