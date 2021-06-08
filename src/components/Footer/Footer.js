5119434cffd960684ec8a4d91b37d843738e9537
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-06-08 11:34:39 +0500

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,12 +7,12 @@ const Footer = () => {
         <footer>
             <Container>
                 <div className="footer">
-            <div className="d-flex1 flex-column">
+            <div className="display-flex d-flex1 flex-column">
                 <h3>Muhammad Siddiq</h3>
                 <div style={{color: "#000", backgroundColor: "#000", width: "150px", height: "2px", margin: "0 2px", borderRadius: "5px"}}></div>
                 <p>I was born in 2006 year, 18 February. I study at Najot Ta'lim in Tashkent.</p>
             </div>
-            <div className="d-flex1 flex-column">
+            <div className="display-flex d-flex1 flex-column">
                 <h4>Links</h4>
                 <Link to="/">Home</Link>
                 <Link to="/about">
@@ -21,7 +21,7 @@ const Footer = () => {
                 <Link to="/portfolio">Portfolio</Link>
                 <Link to="/contact">Contact</Link>
             </div>
-            <div className="d-flex1 flex-column">
+            <div className="display-flex d-flex1 flex-column">
                 <h4>Contact</h4>
                 <p><i className="fas fa-home mr-2"></i>Tashkent</p>
                 <p><i class="fas fa-envelope mr-2"></i>breakingreport4@gmail.com</p>
